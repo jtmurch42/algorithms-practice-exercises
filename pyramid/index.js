2b193d282dd5c196ea6577990b89c97fcfc7b36c
Jeremy Murchison <jtmurch92@gmail.com>
2020-09-29 14:56:08 -0700

    Update pyramid function

--- pyramid/index.js
+++ pyramid/index.js
@@ -23,16 +23,16 @@
  * @param {number} n
  */
 function pyramid(n) {
-  const midPoint = Math.floor((n * 2 - 1) / 2);
+  const midpoint = Math.floor((n * 2 - 1) / 2);
 
   for (let row = 0; row < n; row++) {
     let level = '';
 
     for (let col = 0; col < n * 2 - 1; col++) {
-      if (col < midPoint - row || col > midPoint + row) {
-        level += ' ';
-      } else {
+      if (col >= midpoint - row && col <= midpoint + row) {
         level += '#';
+      } else {
+        level += ' ';
       }
     }
 
